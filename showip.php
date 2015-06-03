75a6bffdc6afb8925272d862119908294464cf28
tarheelpro <chandrav.email@gmail.com>
2015-06-02 21:26:14 -0400

    new file

---
+++ showip.php
@@ -0,0 +1,8 @@
+<?php
+// client-ip.php : Demo script by nixCraft <www.cyberciti.biz>
+// get an IP address
+$ip = $_SERVER['REMOTE_ADDR'];
+// display it back
+echo "<h2>Welcome to CiscoLive 2015</h2>";
+echo "<h3>Your IP address :</h3> " . $ip;
+?>
