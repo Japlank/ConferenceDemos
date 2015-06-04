67ec160c5362914737aff8b1e7c8020fed76a4cb
tarheelpro <chandrav.email@gmail.com>
2015-06-04 10:37:12 -0400

    ip address

--- showip.php
+++ showip.php
@@ -1,8 +1,9 @@
 <?php
-// client-ip.php : Demo script by nixCraft <www.cyberciti.biz>
-// get an IP address
-$ip = $_SERVER['REMOTE_ADDR'];
+$exec = exec("hostname"); //the "hostname" is a valid command in both windows and linux
+$hostname = trim($exec); //remove any spaces before and afterhost
+$ip = gethostbyname($hostname);
 // display it back
 echo "<h2>Welcome to CiscoLive 2015</h2>";
-echo "<h3>Your IP address :</h3> " . $ip;
+echo "<h3> Ip Address of this manchine :</h3> " . $hostname;
+echo "<br><h3>Name of this machine :</h3> " . $ip
 ?>
