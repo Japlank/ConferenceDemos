1dfa6975d5cad660b5a4527c444e3fc6117e737f
tarheelpro <chandrav.email@gmail.com>
2015-06-05 12:42:36 -0400

    time stamp and Ip address

--- showip.php
+++ showip.php
@@ -1,9 +1,18 @@
 <?php
 $exec = exec("hostname"); //the "hostname" is a valid command in both windows and linux
 $hostname = trim($exec); //remove any spaces before and afterhost
-$ip = gethostbyname($hostname);
+//$ip = gethostbyname($hostname);
+
+$cmd="ifconfig eth0 | grep 'inet ' | awk '{print $2}'";
+$ip=shell_exec($cmd);
+//$time = strtotime(date('Y-m-d', time()). '00:00:00');
+$time=date("Y-m-d H:i:s");
+
+
 // display it back
 echo "<h2>Welcome to CiscoLive 2015</h2>";
-echo "<h3> Ip Address of this manchine :</h3> " . $hostname;
-echo "<br><h3>Name of this machine :</h3> " . $ip
+echo "<h3> Ip Address of this machine :</h3> " . $ip;
+echo "<br><h3>Name of this machine :</h3> " . $hostname;
+echo "<br><h3>Time :</h3> " . $time;
 ?>
+
